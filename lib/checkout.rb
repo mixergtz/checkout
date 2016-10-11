a0d51a297dc84457077e4e7bc5af434de3287d07
Mixer Gutierrez <mixergutierrez@gmail.com>
2016-10-10 22:34:46 -0500

    Add explanatory comments for Checkout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -4,11 +4,18 @@ require_relative 'cashier'
 # Main file - It takes a set of pricing rules and applies them
 # to a set of scanned products to get the total price
 class Checkout
+  # Example of the expected param
+  # pricing_rules = [
+  #   { product_code: 'VOUCHER', rule_code: '2X1' },
+  #   { product_code: 'TSHIRT', rule_code: 'BULK',
+  #     rule_options: { bulk_price: 19 } }
+  # ]
   def initialize(pricing_rules = [])
     @scanned_products = []
     @rules = pricing_rules
   end
 
+  # Expected param 'VOUCHER'
   def scan(product_code)
     return if Product.find_by_product_code(product_code).nil?
 
